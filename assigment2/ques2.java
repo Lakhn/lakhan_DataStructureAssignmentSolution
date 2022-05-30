f57a338d9c7e5b2092a15a777ef9cf26af129042
lakhan <67182457+Lakhn@users.noreply.github.com>
2022-05-30 21:51:05 +0530

    Update ques2.java

--- assigment2/ques2.java
+++ assigment2/ques2.java
@@ -1,91 +1,87 @@
 package assigment2;
 
 
+public class ques2 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+    };
 
-public class ques2
-{
-   int val;
-   ques2 left, right;
-   
+    static int index;
 
-   ques2(int item)
-   {
-       val = item;
-       left = right = null;
-   }
-}
-class GFG
-{
-   public static ques2 node;
-   static ques2 prevNode = null;
-   static ques2 headNode = null;
- 
-   static void flattenBTToSkewed(ques2 root,
-                                 int order)
-   {
-      
-   
-       if(root == null)
-       {
-           return;
-       }
-      
-     
-       if(order > 0)
-       {
-           flattenBTToSkewed(root.right, order);
-       }
-       else
-       {
-           flattenBTToSkewed(root.left, order);
-       }
-       ques2 rightNode = root.right;
-       ques2 leftNode = root.left;
-      
-       if(headNode == null)
-       {
-           headNode = root;
-           root.left = null;
-           prevNode = root;
-       }
-       else
-       {
-           prevNode.right = root;
-           root.left = null;
-           prevNode = root;
-       }
-   
-       if (order > 0)
-       {
-           flattenBTToSkewed(leftNode, order);
-       }
-       else
-       {
-           flattenBTToSkewed(rightNode, order);
-       }
-   }
-  
-   static void traverseRightSkewed(ques2 root)
-   {
-       if(root == null)
-       {
-           return;
-       }
-       System.out.print(root.val + " ");
-       traverseRightSkewed(root.right);       
-   }
-  
-   public static void main (String[] args)
-   {
-       GFG tree = new GFG();
-       tree.node = new ques2(50);
-       tree.node.left = new ques2(30);
-       tree.node.right = new ques2(60);
-       tree.node.left.left=new ques2(10);
-       tree.node.right.left=new ques2(55);
-       
-       int order = 0;
-       flattenBTToSkewed(node, order);
-       traverseRightSkewed(headNode);
-   }
+    static void storeInorder(Node node, int inorder[]) {
+        if (node == null)
+            return;
+        storeInorder(node.left, inorder);
+        inorder[index] = node.data;
+        index++;
+        storeInorder(node.right, inorder);
+    }
+
+    static int countNodes(Node root) {
+        if (root == null)
+            return 0;
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+
+    static void arrayToBST(int[] arr, Node root) {
+        if (root == null)
+            return;
+
+        arrayToBST(arr, root.left);
+
+        root.data = arr[index];
+        index++;
+
+        arrayToBST(arr, root.right);
+    }
+
+    static void binaryTreeToBST(Node root) {
+        if (root == null)
+            return;
+
+        int n = countNodes(root);
+
+        int arr[] = new int[n];
+
+        storeInorder(root, arr);
+
+        Arrays.sort(arr);
+
+        index = 0;
+        arrayToBST(arr, root);
+    }
+
+    static Node newNode(int data) {
+        Node temp = new Node();
+        temp.data = data;
+        temp.left = null;
+        temp.right = null;
+        return temp;
+    }
+
+    static void printInorder(Node node) {
+        if (node == null)
+            return;
+        printInorder(node.left);
+
+        System.out.print(node.data + " ");
+
+        printInorder(node.right);
+    }
+
+    public static void main(String args[]) {
+        Node root = null;
+        root = newNode(50);
+        root.left = newNode(30);
+        root.right = newNode(60);
+        root.left.left = newNode(10);
+        root.right.right = newNode(55);
+        binaryTreeToBST(root);
+
+        System.out.println("Following is Inorder Traversal of the converted BST: ");
+        printInorder(root);
+
+    }
 }
